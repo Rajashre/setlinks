b3b01806426b1c8f28085e5f282643e2c9cac5ac
myFirstreact <rajgaykar95@gmail.com>
2021-05-16 22:20:44 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const About = () =>{
+    return<h1>hiii this is about page </h1>
+};
+export default About;
---
+++ src/Contact.jsx
@@ -0,0 +1,7 @@
+
+import React from "react";
+
+const Contact =() =>{
+    return<h1>hiii this is about page </h1>
+};
+export default Contact;
